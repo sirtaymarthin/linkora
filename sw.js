14e7b8ae4006912d4a1c06d058ef50ea322edfac
sirtaymarthin <sirtay21@gmail.com>
2026-03-19 01:01:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,18 @@
+const CACHE = 'lv-v99';
 
-self.addEventListener('install', e=>self.skipWaiting())
-self.addEventListener('activate', e=>self.clients.claim())
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
